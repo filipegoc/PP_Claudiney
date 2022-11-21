dd773b6ba33f51610321bb6616821ca222d46e27
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-21 16:38:12 -0300

    Create ex15.c

---
+++ lab05/ex15.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+float triangulo(float a, float b, float c);
+
+float tipoTriangulo(float x, float y, float z);
+
+int main()
+{
+    float lado1, lado2, lado3, ehTriangulo=0; 
+    printf("Digite o primeiro lado do triangulo: ");
+    scanf("%f", &lado1);
+    printf("Digite o segundo lado do triangulo: ");
+    scanf("%f", &lado2);
+    printf("Digite o terceiro lado do triangulo: ");
+    scanf("%f", &lado3);
+    if(lado1>0 && lado2>0 && lado3>0){
+        ehTriangulo= triangulo(lado1, lado2, lado3);   
+    }
+    else{
+        printf("Erro.");
+    }
+    
+    if(ehTriangulo==1){
+        tipoTriangulo(lado1, lado2, lado3);
+    }
+    
+    return 0;
+}
+
+float triangulo(float a, float b, float c){
+    if((a<b+c) && (b<a+c) && (c<a+b)){
+        return 1;
+    }
+    else{
+        return printf("Nao eh triangulo");
+    }
+}
+
+float tipoTriangulo(float x, float y, float z){
+    if(x==y && y==z){
+        return printf("Triangulo Equilatero");;
+    }
+    else if(x==y || x==z || y==z){
+        return printf("Triangulo Isosceles");;
+    }
+    else if(x!=y && x!=z && y!=z){
+        return printf("Triangulo Escaleno");;
+    }   
+    
+}
