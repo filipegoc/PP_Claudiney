2d3ef20d1c4c5a362969092314cd723c7833a752
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-21 16:38:37 -0300

    Create ex17.c

---
+++ lab05/ex17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float soma(int a, int b);
+
+int main()
+{
+    int n1, n2;
+    printf("Digite dois numeros inteiros positivos e vamos somar os numeros entre eles: ");
+    scanf("%d %d", &n1, &n2);
+    soma(n1, n2);
+    return 0;
+}
+
+float soma(int a, int b){
+    int i, s=0;
+    for(i=a+1; i<b; i++){
+        s+=i;
+    }
+    return printf("A soma dos numeros entre %d a %d eh %d", a, b, s);
+}
