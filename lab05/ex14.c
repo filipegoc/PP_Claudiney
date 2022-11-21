7748a16ca11a6a1990ad947b7b693e3f98df5bcb
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-21 16:37:48 -0300

    Create ex14.c

---
+++ lab05/ex14.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+float consumoTotal(float d, float l);
+
+int main()
+{
+    float distancia, litros; 
+    printf("Digite quantos km voce andou: ");
+    scanf("%f", &distancia);
+    printf("Digite quantos litros voce gastou: ");
+    scanf("%f", &litros);
+    consumoTotal(distancia, litros);
+    return 0;
+}
+
+float consumoTotal(float d, float l){
+    if(d/l >0 && d/l<8){
+        return printf("Consumo de %.2f Km/L, Venda o carro!", d/l);;
+    }
+    else if(d/l>=8 && d/l<=14){
+        return printf("Consumo de %.2f Km/L, Economico!", d/l);;
+    }
+    else if(d/l>14){
+        return printf("Consumo de %.2f Km/L, Super economico!", d/l);;
+    }
+    else{
+        printf("Algo de errado nao esta certo");
+    }
+    
+}
