87e48113193722f79fa17f856a75281d7afb3da8
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-21 16:42:00 -0300

    Create ex24.c

---
+++ lab05/ex24.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int triangulo(int a);
+
+int main()
+{
+    int altura;
+    printf("Digite a altura que voce quer no triangulo: ");
+    scanf("%d", &altura);
+    if(altura>0){
+        triangulo(altura);
+    }
+    else{
+        printf("Erro.");
+    }
+    return 0;
+}
+
+int triangulo(int a){
+    int i, j, k;
+    for(i=1; i<=a; i++){
+        for(j=1; j<=(i*2); j++){
+            if(j!=1){
+                printf("*");        
+            }
+            for(k=i; k<a; k++){
+                if(j==1){
+                    printf(" "); 
+                }
+            }
+        }
+        if(i!=a){
+            printf("\n");   
+        }
+    }
+}
