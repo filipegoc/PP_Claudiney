e6537a30bfde9c8a8399ec93d92c3acf5e51288f
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-25 20:56:29 -0300

    Update ex21.c

--- lab05/ex21.c
+++ lab05/ex21.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int exclamacao(int a);
+void exclamacao(int a);
 
 int main()
 {
@@ -13,11 +13,10 @@ int main()
     else{
         printf("Erro.");
     }
-    
     return 0;
 }
 
-int exclamacao(int a){
+void exclamacao(int a){
     int i, j;
     for(i=1; i<=a; i++){
         for(j=1; j<=i; j++){
