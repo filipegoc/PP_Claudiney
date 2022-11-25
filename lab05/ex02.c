eb1118f98fe0bc6f42413fcaea6dfc524f49289f
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-25 07:46:15 -0300

    Update ex02.c

--- lab05/ex02.c
+++ lab05/ex02.c
@@ -1,49 +1,55 @@
 #include <stdio.h>
 
-int dataExtensa(int d, int m, int a){
-    if(m==1){
-        printf("%d de janeiro de %d", d, a);
-    }
-    if(m==2){
-        printf("%d de fevereiro de %d", d, a);
-    }
-    if(m==3){
-        printf("%d de marco de %d", d, a);
-    }
-    if(m==4){
-        printf("%d de abril de %d", d, a);
-    }
-    if(m==5){
-        printf("%d de maio de %d", d, a);
-    }
-    if(m==6){
-        printf("%d de junho de %d", d, a);
-    }
-    if(m==7){
-        printf("%d de julho de %d", d, a);
-    }
-    if(m==8){
-        printf("%d de agosto de %d", d, a);
-    }
-    if(m==9){
-        printf("%d de setembro de %d", d, a);
-    }
-    if(m==10){
-        printf("%d de outubro de %d", d, a);
-    }
-    if(m==11){
-        printf("%d de novembro de %d", d, a);
-    }
-    if(m==12){
-        printf("%d de dezembro de %d", d, a);
-    }
-}
+void dataExtensa(int m);
 
 int main()
 {
     int dia, mes, ano, extenso;
     printf("Digite uma data (dd/mm/aaaa) e vamos exibi-la por extenso: ");
     scanf("%d/%d/%d", &dia, &mes, &ano);
-    extenso= dataExtensa(dia, mes, ano);
+    printf("%d de ", dia);
+    dataExtensa(mes);
+    printf(" de %d", ano);
     return 0;
+}
+
+void dataExtensa(int m){
+    switch(m){
+        case 1:
+            printf("janeiro");
+            break;
+        case 2:
+            printf("fevereiro");
+            break;
+        case 3:
+            printf("marco");
+            break;
+        case 4:
+            printf("abril");
+            break;
+        case 5:
+            printf("maio");
+            break;
+        case 6:
+            printf("junho");
+            break;
+        case 7:
+            printf("julho");
+            break;
+        case 8:
+            printf("agosto");
+            break;
+        case 9:
+            printf("setembro");
+            break;
+        case 10:
+            printf("outubro");
+            break;
+        case 11:
+            printf("novembro");
+            break;
+        case 12:
+            printf("dezembro");
+            break;
+    }
 }
