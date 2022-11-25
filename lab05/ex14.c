3df930488763fbb27bb02f95f4c45e504aa1bdb3
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-25 20:35:54 -0300

    Update ex14.c

--- lab05/ex14.c
+++ lab05/ex14.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float consumoTotal(float d, float l);
+void consumoTotal(float d, float l);
 
 int main()
 {
@@ -13,15 +13,15 @@ int main()
     return 0;
 }
 
-float consumoTotal(float d, float l){
+void consumoTotal(float d, float l){
     if(d/l >0 && d/l<8){
-        return printf("Consumo de %.2f Km/L, Venda o carro!", d/l);;
+        printf("Consumo de %.2f Km/L, Venda o carro!", d/l);;
     }
     else if(d/l>=8 && d/l<=14){
-        return printf("Consumo de %.2f Km/L, Economico!", d/l);;
+        printf("Consumo de %.2f Km/L, Economico!", d/l);;
     }
     else if(d/l>14){
-        return printf("Consumo de %.2f Km/L, Super economico!", d/l);;
+        printf("Consumo de %.2f Km/L, Super economico!", d/l);;
     }
     else{
         printf("Algo de errado nao esta certo");
