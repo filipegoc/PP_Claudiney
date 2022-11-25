9fec9de0f733e04ffed1292dd89939ba7911d2db
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-25 08:05:05 -0300

    Update ex11.c

--- lab05/ex11.c
+++ lab05/ex11.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int media(float x, float y, float z, char a);
+void media(float x, float y, float z, char a);
 
 int main()
 {
@@ -10,18 +10,18 @@ int main()
     scanf("%f %f %f", &n1, &n2, &n3);
     printf("Digite 'A' para Media Aritmetica e 'P' para Media Ponderada: ");
     scanf(" %c", &letra);
-    mediaFinal= media(n1, n2, n3, letra);
+    media(n1, n2, n3, letra);
     return 0;
 }
 
-int media(float x, float y, float z, char a){
+void media(float x, float y, float z, char a){
     if(a=='A' || a=='a'){
-        return printf("A Media Aritmetica eh %.2f", (x+y+z)/3);
+        printf("A Media Aritmetica eh %.2f", (x+y+z)/3);
     }
-    if(a=='P' || a=='p'){
-        return printf("A Media Ponderada eh %.2f", (x*5+y*3+z*2)/10);
+    else if(a=='P' || a=='p'){
+        printf("A Media Ponderada eh %.2f", (x*5+y*3+z*2)/10);
     }
     else{
-        return printf("Opcao Invalida!");
+        printf("Opcao Invalida!");
     }
 }
