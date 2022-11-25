bd91036e06f18b0a02bb0e75790b401ffa0903c0
filipegoc <104858798+filipegoc@users.noreply.github.com>
2022-11-25 07:55:02 -0300

    Update ex09.c

--- lab05/ex09.c
+++ lab05/ex09.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <math.h>
 
-float VolumeCilindro(float h, float r){
-    float pi=3.141592;
-    return pi*r*r*h;
-}
+float VolumeCilindro(float h, float r);
 
 int main()
 {
@@ -12,6 +8,11 @@ int main()
     printf("Digite o valor da altura e raio e vamos calcular o volume do cilindo: ");
     scanf("%f %f", &altura, &raio);
     volume= VolumeCilindro(altura, raio);
-    printf("O valor da hipotenusa eh %.2f", volume);
+    printf("O valor do volume eh %.2f", volume);
     return 0;
+}
+
+float VolumeCilindro(float h, float r){
+    float pi=3.141592;
+    return pi*r*r*h;
 }
